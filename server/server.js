f63f942828f84a60bb78fafe135b4ece12c62722
Steven Lee <stevenyslee@gmail.com>
2018-12-28 15:51:30 -0800

    Create register endpoint

--- server/server.js
+++ server/server.js
@@ -5,4 +5,6 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// app.get('/register', (req, res) => res.send('Register!'));
+
 app.listen(3000, () => console.log('App listening on port 3000'));
