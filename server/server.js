caeaee4cd2b761e004a9fa2bc38dc0612ac721c9
Steven Lee <stevenyslee@gmail.com>
2018-12-28 16:13:12 -0800

    Recieve body successfully

--- server/server.js
+++ server/server.js
@@ -3,10 +3,16 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/register', (req, res) => res.send('Register!'));
+app.post('/register/', (req, res) => {
+  console.log(req.body);
+  res.send('Register!');
+});
 
 app.listen(3000, () => console.log('App listening on port 3000'));
