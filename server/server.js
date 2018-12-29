7ce95b54bba55a2089933f1df13b9f567bf10e66
Steven Lee <stevenyslee@gmail.com>
2018-12-28 16:09:48 -0800

    Add body-parser

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// app.get('/register', (req, res) => res.send('Register!'));
+app.post('/register', (req, res) => res.send('Register!'));
 
 app.listen(3000, () => console.log('App listening on port 3000'));
