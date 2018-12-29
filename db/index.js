ad979f2ef75e91a7d0f4903b73ae19df2e002191
Steven Lee <stevenyslee@gmail.com>
2018-12-28 17:37:12 -0800

    Authorize Users

--- db/index.js
+++ db/index.js
@@ -2,25 +2,34 @@ const { mongoUser, mongoPassword } = require('./../config.js');
 const MongoClient = require('mongodb').MongoClient;
 
 
-const registerUser = ({ username, password }) => {
-  MongoClient.connect(`mongodb://${mongoUser}:${mongoPassword}@ds245234.mlab.com:45234/sinwaves`, (err, db) => {
+const registerUser = ({ username, password }, cb) => {
+  MongoClient.connect(`mongodb://${mongoUser}:${mongoPassword}@ds245234.mlab.com:45234/sinwaves`, { useNewUrlParser: true }, (err, db) => {
     if(!err) {
-      console.log("We are connected");
+      console.log('We are connected');
       let dbo = db.db('sinwaves');
       let myobj = { username, password };
-      dbo.collection("users").insertOne(myobj, (err, res) => {
-        if (err) throw err;
-        console.log("1 document inserted");
+      dbo.collection('users').insertOne(myobj, (err, res) => {
+        if (err) console.log(err);
+        console.log('1 document inserted');
         db.close();
+        cb();
       });
     }
   });
 }
 
-const validateUser = () => {
-  MongoClient.connect(`mongodb://${mongoUser}:${mongoPassword}@ds245234.mlab.com:45234/sinwaves`, function(err, db) {
+const validateUser = ({ username, password }, cb) => {
+  MongoClient.connect(`mongodb://${mongoUser}:${mongoPassword}@ds245234.mlab.com:45234/sinwaves`, { useNewUrlParser: true }, (err, db) => {
     if(!err) {
-      console.log("We are connected");
+      console.log('We are connected');
+      let dbo = db.db('sinwaves');
+      let query = { username };
+      dbo.collection("users").find(query).toArray((err, res) => {
+        if (err) console.log(err);
+        console.log(res);
+        db.close();
+        cb(res);
+      });
     }
   });
 };
