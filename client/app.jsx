46ca63f40d7282ecc9382fc3ef243c2fecb90edb
Steven Lee <stevenyslee@gmail.com>
2018-12-28 16:09:20 -0800

    Add username and password validation

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from 'axios';
 
 const Login = () => {
   return (
@@ -30,7 +31,7 @@ const Register = ({ registerUser, handleUsernameChange, handlePasswordChange }) 
           <input type="text" name="username" onChange={(e) => {handleUsernameChange(e)}}/>
           <br/>
           <p>Password</p>
-          <input type="text" name="password" onClick={(e) => {handlePasswordChange(e)}}/>
+          <input type="password" name="pass" onChange={(e) => {handlePasswordChange(e)}}/>
         </label>
         <br />
         <button onClick={(e) => {registerUser(e)}}>Submit</button>
@@ -54,18 +55,21 @@ class App extends React.Component {
       isAuth: false,
     };
 
+    this.loginUser = this.loginUser.bind(this);
     this.registerUser = this.registerUser.bind(this);
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
   }
 
   handleUsernameChange(e) {
+    console.log(e.target.value);
     this.setState({
       username: e.target.value
     });
   }
 
   handlePasswordChange(e) {
+    console.log(e.target.value);
     this.setState({
       password: e.target.value
     });
@@ -73,7 +77,22 @@ class App extends React.Component {
 
   registerUser(e) {
     e.preventDefault();
+    const validationRegex = /^[a-zA-Z0-9]+$/;
     
+    if (this.state.username.match(validationRegex) && this.state.password.match(validationRegex)) {
+      axios.post('/register/', {
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    } else {
+      alert('Please use Alphanumerical values for username and password!');
+    }
   }
 
   loginUser() {
@@ -88,11 +107,15 @@ class App extends React.Component {
         <span> </span>
         <Link to="/register/">Register</Link>
 
-        <Route path="/login/" component={Login} />
+        <Route path="/login/" render={props => <Login 
+          loginUser={this.loginUser} 
+          handleUsernameChange={this.handleUsernameChange}
+          handlePasswordChange={this.handlePasswordChange}
+          />} />
         <Route path="/register/" render={props => <Register 
           registerUser={this.registerUser} 
-          handleUsernameChange={handleUsernameChange}
-          handlePasswordChange={handlePasswordChange}
+          handleUsernameChange={this.handleUsernameChange}
+          handlePasswordChange={this.handlePasswordChange}
           />} />
         <Route path="/profile/" component={Profile} />
         </div>
